dafa18d35637db691d6675ff9b30fc1993de289c
augustinetharakan12 <augustinetharakan12@gmail.com>
2018-04-18 10:35:17 +0530

    removed all the redundant code

--- app/models.py
+++ app/models.py
@@ -18,29 +18,3 @@ class user_company(models.Model):
 	no=models.IntegerField(null=True)
 	def __str__(self):
 		return self.email +' '+ self.company_code
-
-
-# class alumni(models.Model):
-#     fname=models.CharField(max_length=200,null=True)
-#     lname=models.CharField(max_length=200,null=True)
-#     year_pass=models.IntegerField(null=True)
-#     id = models.IntegerField(primary_key=True)
-#     #student=models.CharField(max_length=2,null=True)#y or N
-#     #inst_name=models.CharField(max_length=200,null=True)
-#     phno=models.CharField(max_length=15,null=True)
-#     email=models.CharField(max_length=100,null=True,blank=True)
-#     #dob=models.DateField
-#     # address info
-#     #a_street = models.CharField(max_length=200,null=True)
-#     #a_city = models.CharField(max_length=200,null=True)
-#     #a_state = models.CharField(max_length=200,null=True)
-#     #a_country = models.CharField(max_length=200,null=True)
-#     #a_pin = models.CharField(max_length=50,null=True)
-#     #event info
-#     #event=models.CharField(max_length=30,default='none')
-
-#     no_attending=models.IntegerField(default=0)
-#     attend=models.IntegerField(default=0)
-#     registration_fee=models.IntegerField(default=0)
-
-    # def __str__(self):
