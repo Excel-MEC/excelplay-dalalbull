e5d175e8d1e595d0743e9c9b5d58aec7f20c307a
augustine <augustinetharakan12@gmail.com>
2018-10-22 22:11:44 +0530

    asgi code updates

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,3 +4,4 @@ python excelplay-dalalbull/manage.py makemigrations && \
 	python excelplay-dalalbull/manage.py migrate
 cd excelplay-dalalbull
 gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8002
+daphne -b 0.0.0.0 -p 8002 excelplay_dalalbull.asgi:application
