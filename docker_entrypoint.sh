2a36363d6cbe1e7323a3c83a835d2894966b313a
augustine <augustinetharakan12@gmail.com>
2018-10-23 01:04:13 +0530

    bug

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python excelplay_dalalbull/excelplay-dalalbull/manage.py makemigrations && \
-	python excelplay-dalalbull/excelplay-dalalbull/manage.py migrate
+python excelplay-dalalbull/excelplay_dalalbull/manage.py makemigrations && \
+	python excelplay-dalalbull/excelplay_dalalbull/manage.py migrate
 cd excelplay-dalalbull/excelplay_dalalbull
 gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8002
 daphne -b 0.0.0.0 -p 8002 excelplay_dalalbull.asgi:application
