f673a782c8e766f92ea51154b30c0faf27ff4835
augustine <augustinetharakan12@gmail.com>
2018-10-22 22:52:23 +0530

    docker file update

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python excelplay-dalalbull/manage.py makemigrations && \
+python excelplay_dalalbull/manage.py makemigrations && \
 	python excelplay-dalalbull/manage.py migrate
-cd excelplay-dalalbull
+cd excelplay-dalalbull/excelplay_dalalbull
 gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8002
 daphne -b 0.0.0.0 -p 8002 excelplay_dalalbull.asgi:application
