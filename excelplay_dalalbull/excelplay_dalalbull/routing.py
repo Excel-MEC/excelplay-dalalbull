b9cb043b6b145ce66e24da7d73600dcf7d527a43
augustine <augustinetharakan12@gmail.com>
2018-10-24 15:03:08 +0530

    ChannelNameRouter update

--- excelplay_dalalbull/excelplay_dalalbull/routing.py
+++ excelplay_dalalbull/excelplay_dalalbull/routing.py
@@ -5,13 +5,21 @@ from django.urls import path
 from api.consumers import LeaderBoard, Portfolio, nifty_channel, graph_data_channel, ticker_data_channel, sell_channel
 
 application = ProtocolTypeRouter({
-		"websocket":ChannelNameRouter([
+		"websocket":URLRouter([
 				path("leaderboard-channel/",LeaderBoard),
 				path("portfolio-channel/",Portfolio),
 				path("nifty-channel/",nifty_channel),
 				path("graph-channel/",graph_data_channel),
 				path("ticker-channel/",ticker_data_channel),
 				path("sell-channel/",sell_channel),
+			]),
+		"channel":ChannelNameRouter([
+				"leaderboard-channel":LeaderBoard,
+				"portfolio-channel":Portfolio,
+				"nifty-channel":nifty_channel,
+				"graph-channel":graph_data_channel,
+				"ticker-channel":ticker_data_channel,
+				"sell-channel":sell_channel,
 			])
-
 	})
+
