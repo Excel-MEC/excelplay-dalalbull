12ed2fa0cf6de756f8be4e104e22339fa8477e3e
augustine <augustinetharakan12@gmail.com>
2018-10-28 21:11:11 +0530

    migration command

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python excelplay-dalalbull/excelplay_dalalbull/manage.py makemigrations api 
 python excelplay-dalalbull/excelplay_dalalbull/manage.py makemigrations && \
 	python excelplay-dalalbull/excelplay_dalalbull/manage.py migrate
 cd excelplay-dalalbull/excelplay_dalalbull
