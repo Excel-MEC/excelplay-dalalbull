7790494311244ce30da13af89e38dfe56fa3f1ec
Kurian Benoy <kurian.bkk@gmail.com>
2019-02-21 23:01:35 +0530

    Add initial test config

--- excelplay_dalalbull/api/tests.py
+++ excelplay_dalalbull/api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserCase(TestCase):
+    def setUp(self):
+        User.objects.create(id="111")
+        User.objects.create(id="231")
