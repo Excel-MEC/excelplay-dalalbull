d5cf0787967029a41a6b43f6da191b07cdcd0f0d
root <root@excelplay.asia-south1-c.c.excel-2018.internal>
2019-09-18 14:31:59 +0000

    celery, daphne temporary fix

--- runcelery.sh
+++ runcelery.sh
@@ -1,3 +1,4 @@
 cd excelplay-dalalbull/excelplay_dalalbull
 echo Starting celery
+pip install kombu==4.3.0
 celery -A excelplay_dalalbull worker -l info -B
--- rundaphne.sh
+++ rundaphne.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd excelplay-dalalbull/excelplay_dalalbull
 echo Running Daphne
+pip install channels daphne==2.2.5
 daphne -b 0.0.0.0 -p 8003 excelplay_dalalbull.asgi:application
