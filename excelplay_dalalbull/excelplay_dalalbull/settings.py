962494f3e571ef969fc25b0e378dd1cebdcdecf3
Augustine <augustinetharakan12@gmail.com>
2019-10-07 18:50:08 +0530

    changed django static files location

--- excelplay_dalalbull/excelplay_dalalbull/settings.py
+++ excelplay_dalalbull/excelplay_dalalbull/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
 
 from datetime import timedelta
 
